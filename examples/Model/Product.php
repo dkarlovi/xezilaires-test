25dcb502f3fb8f0d02abfa0ad855782a344b1104
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2019-08-13 09:52:46 +0200

    annotations example

--- examples/Model/Product.php
+++ examples/Model/Product.php
@@ -4,10 +4,15 @@ namespace Example\Model;
 
 use Xezilaires\Annotation as XLS;
 
+/**
+ * @XLS\Options(header=1, start=2)
+ */
 class Product implements \JsonSerializable
 {
     /**
      * @var string
+     *
+     * @XLS\HeaderReference(header="Name")
      */
     private $name;
 
---
+++ examples/annotations.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Example;
+
+use Symfony\Component\Serializer\Serializer;
+use Xezilaires\Metadata;
+use Example\Model\Product;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+// needed only for the demo
+// <demo>
+$normalizer = new \Xezilaires\Bridge\Symfony\Serializer\ObjectSerializer(
+    new Serializer([
+        new \Symfony\Component\Serializer\Normalizer\PropertyNormalizer(),
+    ])
+);
+$iteratorFactory = new \Xezilaires\SpreadsheetIteratorFactory($normalizer);
+$annotationDriver = new Metadata\Annotation\AnnotationDriver();
+// </demo>
+
+$iterator = $iteratorFactory->createFromPath(
+    // https://github.com/dkarlovi/xezilaires/raw/master/resources/fixtures/products.xlsx
+    new \SplFileObject(__DIR__.'/../products.xlsx'),
+    $annotationDriver->getMetadataMapping(Product::class)
+);
+
+$out = iterator_to_array($iterator);
+echo \json_encode($out, JSON_PRETTY_PRINT);
